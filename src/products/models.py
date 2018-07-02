bde061c03151ff9d1660e9768edc97100c76b2e2
CFE <hello@teamcfe.com>
2018-07-02 15:16:18 -0700

    11 - Change a Model

--- src/products/models.py
+++ src/products/models.py
@@ -5,4 +5,5 @@ class Product(models.Model):
     title       = models.CharField(max_length=120) # max_length = required
     description = models.TextField(blank=True, null=True)
     price       = models.DecimalField(decimal_places=2, max_digits=10000)
-    summary     = models.TextField()
+    summary     = models.TextField(blank=False, null=False)
+    featured    = models.BooleanField() # null=True, default=True
