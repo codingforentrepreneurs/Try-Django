f03107b9457feea724a905484a2758b9835762f3
CFE <hello@teamcfe.com>
2018-07-02 15:05:58 -0700

    10 - New Model Fields

--- src/products/models.py
+++ src/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title       = models.TextField()
-    description = models.TextField()
-    price       = models.TextField()
-    summary     = models.TextField(default='this is cool!')
+    title       = models.CharField(max_length=120) # max_length = required
+    description = models.TextField(blank=True, null=True)
+    price       = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary     = models.TextField()
