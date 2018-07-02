ba47ae15eab8b90ca43a05e8b75c0723131c298c
CFE <hello@teamcfe.com>
2018-07-02 15:47:07 -0700

    14 - Django Templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,16 +5,15 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs): # *args, **kwargs
     print(args, kwargs)
     print(request.user)
-    return HttpResponse("<h1>Hello World</h1>") # string of HTML code
-
+    #return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+    return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
-
+    return render(request, "contact.html", {})
 
 
 def about_view(request, *args, **kwargs):
-    return HttpResponse("<h1>About Page</h1>")
+    return render(request, "about.html", {})
 
 
 def social_view(request, *args, **kwargs):
--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
