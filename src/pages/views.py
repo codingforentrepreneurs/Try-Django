d2a9d7711f55d35bc2d6ceb9409de4cf6a46ee9f
CFE <hello@teamcfe.com>
2018-07-05 14:36:54 -0700

    20 - Template Tags and Filters

--- src/pages/views.py
+++ src/pages/views.py
@@ -14,10 +14,11 @@ def contact_view(request, *args, **kwargs):
 
 def about_view(request, *args, **kwargs):
     my_context = {
-        "my_text": "This is about us",
+        "title": "abc this is about us",
         "this_is_true": True,
         "my_number": 123,
-        "my_list": [1313, 4231, 312, "Abc"]
+        "my_list": [1313, 4231, 312, "Abc"],
+        "my_html": "<h1>Hello World</h1>"
 
     }
     return render(request, "about.html", my_context)
