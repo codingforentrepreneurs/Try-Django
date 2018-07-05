8a7b45bee835e79d5e5a9f17cbbd391e2f01e974
CFE <hello@teamcfe.com>
2018-07-05 14:07:00 -0700

    17 - Rendering Context in a Template & 18 - For Loop in a Template

--- src/pages/views.py
+++ src/pages/views.py
@@ -13,7 +13,13 @@ def contact_view(request, *args, **kwargs):
 
 
 def about_view(request, *args, **kwargs):
-    return render(request, "about.html", {})
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [1313, 4231, 312, "Abc"]
+
+    }
+    return render(request, "about.html", my_context)
 
 
 def social_view(request, *args, **kwargs):
