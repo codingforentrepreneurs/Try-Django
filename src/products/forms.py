324bc7bea21c84c79339948e367854ff87b72fe3
CFE <hello@teamcfe.com>
2018-07-08 14:01:12 -0700

    27 - Form Validation Methods

--- src/products/forms.py
+++ src/products/forms.py
@@ -5,6 +5,23 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title       = forms.CharField(label='', 
+                    widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    email       = forms.EmailField()
+    description = forms.CharField(
+                        required=False, 
+                        widget=forms.Textarea(
+                                attrs={
+                                    "placeholder": "Your description",
+                                    "class": "new-class-name two",
+                                    "id": "my-id-for-textarea",
+                                    "rows": 20,
+                                    'cols': 120
+                                }
+                            )
+                        )
+    price       = forms.DecimalField(initial=199.99)
+    
     class Meta:
         model = Product
         fields = [
@@ -12,6 +29,21 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if not "CFE" in title:
+            raise forms.ValidationError("This is not a valid title")
+        if not "news" in title:
+            raise forms.ValidationError("This is not a valid title")
+        return title
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get("email")
+        if not email.endswith("edu"):
+            raise forms.ValidationError("This is not a valid email")
+        return email
+            
 
 
 
--- src/products/views.py
+++ src/products/views.py
@@ -6,20 +6,20 @@ from .models import Product
 # Create your views here.
 
 
-def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == "POST":
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            # now the data is good
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
-    context = {
-        "form": my_form
-    }
-    return render(request, "products/product_create.html", context)
+# def product_create_view(request):
+#     my_form = RawProductForm()
+#     if request.method == "POST":
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             # now the data is good
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
+#     context = {
+#         "form": my_form
+#     }
+#     return render(request, "products/product_create.html", context)
 
 # def product_create_view(request):
 #     #print(request.GET)
@@ -32,16 +32,16 @@ def product_create_view(request):
 #     return render(request, "products/product_create.html", context)
 
 
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm()
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
 
-#     context = {
-#         'form': form
-#     }
-#     return render(request, "products/product_create.html", context)
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
 
 
 def product_detail_view(request):
