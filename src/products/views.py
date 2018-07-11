d4fb754fb4063e44413b3884fc3d93394320161d
CFE <hello@teamcfe.com>
2018-07-11 12:36:56 -0700

    30 - Handle DoesNotExist

--- src/products/views.py
+++ src/products/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def dynamic_lookup_view(request, id):
-    obj = Product.objects.get(id=id)
+    #obj = Product.objects.get(id=id)
+    obj = get_object_or_404(Product, id=id)
+    # obj = Product.objects.get(id=id)
+    # try:
+    #     obj = Product.objects.get(id=id)
+    # except Product.DoesNotExist:
+    #     raise Http404
     context = {
         "object": obj
     }
     return render(request, "products/product_detail.html", context)
-
 
 
 
