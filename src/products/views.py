cdc250281b9acc19dac7f1f916717761c9db5003
CFE <hello@teamcfe.com>
2018-07-11 12:33:20 -0700

    29 - Dynamic URL Routing

--- src/products/views.py
+++ src/products/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
+from .models import Product
+
+def dynamic_lookup_view(request, id):
+    obj = Product.objects.get(id=id)
+    context = {
+        "object": obj
+    }
+    return render(request, "products/product_detail.html", context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 from .forms import ProductForm, RawProductForm
 
-from .models import Product
-# Create your views here.
 
 def render_initial_data(request):
     initial_data = {
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -17,9 +17,30 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view, about_view
-from products.views import product_detail_view, product_create_view, render_initial_data
+from products.views import (
+    product_detail_view, 
+    product_create_view, 
+    render_initial_data,
+    dynamic_lookup_view
+    )
 
 urlpatterns = [
+    path('products/<int:id>/', dynamic_lookup_view, name='product'),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     path('', home_view, name='home'),
     path('about/', about_view),
     path('contact/', contact_view),
