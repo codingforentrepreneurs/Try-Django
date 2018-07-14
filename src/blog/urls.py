12e23f0f75cc0b7e1bac778e8ad160d03f218d34
CFE <hello@teamcfe.com>
2018-07-14 13:23:57 -0700

    38 - Class Based Views - CreateView and UpdateView

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 from .views import (
+    ArticleCreateView,
     ArticleDetailView,
-    ArticleListView
+    ArticleListView,
+    ArticleUpdateView
 
 )
 
 app_name = 'articles'
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-list'),
-    #path('create/', view_name, name='article-create'),
+    path('create/', ArticleCreateView.as_view(), name='article-create'),
     path('<int:id>/', ArticleDetailView.as_view(), name='article-detail'),
-    #path('<int:id>/update/', view_name, name='article-update'),
+    path('<int:id>/update/', ArticleUpdateView.as_view(), name='article-update'),
     #path('<int:id>/delete/', view_name, name='article-delete'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -9,8 +9,23 @@ from django.views.generic import (
     DeleteView
 )
 
-
+from .forms import ArticleModelForm
 from .models import Article
+
+class ArticleCreateView(CreateView):
+    template_name = 'articles/article_create.html'
+    form_class = ArticleModelForm
+    queryset = Article.objects.all() # <blog>/<modelname>_list.html
+    #success_url = '/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+    #def get_success_url(self):
+    #    return '/'
+
+
 
 class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
@@ -19,11 +34,24 @@ class ArticleListView(ListView):
 
 class ArticleDetailView(DetailView):
     template_name = 'articles/article_detail.html'
-    queryset = Article.objects.filter(id__gt=1)
+    #queryset = Article.objects.all()
 
-    # def get_object(self):
-    #     id_ = self.kwargs.get("id")
-    #     return get_object_or_404(Article, id=id_)
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
 
 
+
+class ArticleUpdateView(UpdateView):
+    template_name = 'articles/article_create.html'
+    form_class = ArticleModelForm
+    queryset = Article.objects.all()
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
 
