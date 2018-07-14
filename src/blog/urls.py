51020e500ea82b7aeaceb1e8323b147f3e5dd7a4
CFE <hello@teamcfe.com>
2018-07-14 13:32:30 -0700

    39 - Class Based Views - DeleteView

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
     ArticleCreateView,
+    ArticleDeleteView,
     ArticleDetailView,
     ArticleListView,
-    ArticleUpdateView
+    ArticleUpdateView,
+   
 
 )
 
@@ -13,5 +15,5 @@ urlpatterns = [
     path('create/', ArticleCreateView.as_view(), name='article-create'),
     path('<int:id>/', ArticleDetailView.as_view(), name='article-detail'),
     path('<int:id>/update/', ArticleUpdateView.as_view(), name='article-update'),
-    #path('<int:id>/delete/', view_name, name='article-delete'),
+    path('<int:id>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,6 @@
+
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from django.views.generic import (
     CreateView,
@@ -25,8 +27,6 @@ class ArticleCreateView(CreateView):
     #def get_success_url(self):
     #    return '/'
 
-
-
 class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
     queryset = Article.objects.all() # <blog>/<modelname>_list.html
@@ -41,11 +41,9 @@ class ArticleDetailView(DetailView):
         return get_object_or_404(Article, id=id_)
 
 
-
 class ArticleUpdateView(UpdateView):
     template_name = 'articles/article_create.html'
     form_class = ArticleModelForm
-    queryset = Article.objects.all()
 
     def get_object(self):
         id_ = self.kwargs.get("id")
@@ -54,4 +52,23 @@ class ArticleUpdateView(UpdateView):
     def form_valid(self, form):
         print(form.cleaned_data)
         return super().form_valid(form)
+
+
+class ArticleDeleteView(DeleteView):
+    template_name = 'articles/article_delete.html'
+    
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+
+    # def get_success_url(self):
+    #     return reverse('articles:article-list')
+
+
+
+
+
+
+
+
 
