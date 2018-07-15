1b25f0050e83717a373a080b5b0846e5c4a93710
CFE <hello@teamcfe.com>
2018-07-15 16:33:02 -0700

    41 - Raw Detail Class Based View

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     
 
     # path('create/', <create_view>, name='courses-create'),
-    # path('<int:id>/', CourseView.as_view(), name='courses-detail'),
+    path('<int:id>/', CourseView.as_view(), name='courses-detail'),
     # path('<int:id>/update/', <update_view>, name='courses-update'),
     # path('<int:id>/delete/', <delete_view>, name='courses-delete'),
 ]
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
-
+from .models import Course 
 # BASE VIEW CLass = VIEW
 
 class CourseView(View):
-    template_name = "about.html"
-    def get(self, request, *args, **kwargs):
+    template_name = "courses/course_detail.html" # DetailView
+    def get(self, request, id=None, *args, **kwargs):
         # GET method
-        return render(request, self.template_name, {})
+        context = {}
+        if id is not None:
+            obj = get_object_or_404(Course, id=id)
+            context['object'] = obj
+        return render(request, self.template_name, context)
 
     # def post(request, *args, **kwargs):
     #     return render(request, 'about.html', {})
